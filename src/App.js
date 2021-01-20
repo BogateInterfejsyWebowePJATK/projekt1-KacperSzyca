33ab9b70922b3d774e0e224b732a97e6c13b7dcd
kacperszyca <kacperszyca11@gmail.com>
2021-01-21 00:28:50 +0100

    Zad3

--- src/App.js
+++ src/App.js
@@ -1,85 +1,54 @@
 import React, {useRef, useState} from 'react';
+import Users from "./data/users"
 import './App.css';
 
 function App() {
-    const [newText, setNewText] = useState("");
-    const [newText2, setNewText2] = useState("");
-    const [newRadioButton, setNewRadioButton] = useState("");
-    const [newCheck, setNewCheck] = useState("");
-    const [newDropDown, setNewDropDown] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [data] = useState(Users);
 
-    const text = useRef();
-    const text2 = useRef();
-    const radioButton = useRef();
-    const check = useRef();
-    const dropDown = useRef();
+    const handleEmail = event =>
+    {
+        setEmail(event.target.value);
+    }
 
-    const submit = event => {
+    const handlePassword = event =>
+    {
+        setPassword(event.target.value);
+    }
+
+    const handleLogin = event =>
+    {
         event.preventDefault();
-        setNewText(text.current.value);
-        setNewText2(text2.current.value);
-        setNewDropDown(dropDown.current.value);
-        //setNewRadioButton(radioButton.current.value);
+
+        let passAreGood = false;
+
+        data.map(data => {
+            if ( data.email === email && data.password === password)
+            {
+                passAreGood = true;
+            }
+        });
+
+        if(passAreGood)
+        {
+            alert("Udało się zalogować")
+        }
+        else
+            alert("Błędne dane!")
     }
 
-    const handleCheck = (event) => {
-        setNewCheck(event.target.value)
-    }
-
-    const handleRadioButton = (event) => {
-        setNewRadioButton(event.target.value)
-    }
-
-    const reset = event => {
-        event.preventDefault();
-        setNewText("");
-        setNewText2("");
-        setNewRadioButton("");
-        setNewCheck("");
-    }
-
-  return(
-     <div>
-        <form onSubmit={submit}>
-            <input type="text" ref={text}/> <br/>
-            <input type="text" ref={text2}/> <br/>
-            <div onChange={handleRadioButton.bind(this)}>
-                <input type="radio" value="MALE" name="gender"/> Male
-                <input type="radio" value="FEMALE" name="gender"/> Female
-            </div>
-            <br/>
-            <select ref={dropDown}>
-                <option value={"Rower"}>Rower</option>
-                <option value={"Inny Rower"}>Inny rower</option>
-            </select>
-            <br/>
-            <input id={"check"} ref={check} type="checkbox" onClick={handleCheck} value={"prawda"}/> Prawda
-            <br/>
-            <button type={"submit"}>Wyslij</button> <br/>
-            <button type={"reset"} onClick={reset}>Reset</button>
-        </form>
-         <table>
-             <thead>
-                 <tr>
-                     <th>Text</th>
-                     <th>Text2</th>
-                     <th>Radio</th>
-                     <th>Drop down</th>
-                     <th>Checkbox</th>
-                 </tr>
-             </thead>
-             <tbody>
-                 <tr>
-                     <td>{newText}</td>
-                     <td>{newText2}</td>
-                     <td>{newRadioButton}</td>
-                     <td>{newDropDown}</td>
-                     <td>{newCheck}</td>
-                 </tr>
-             </tbody>
-         </table>
-     </div>
-  );
+    return(
+        <div>
+            <form onSubmit={handleLogin}>
+                <input type="text"  onChange={handleEmail} required={true} pattern="[A-Za-z0-9]+@+[a-z0-9]+\.[a-z]+$"/>
+                <br/>
+                <input type="password" onChange={handlePassword} required={true} pattern="[A-Za-z0-9]+$"/>
+                <br/>
+                <button type="submit">Zaloguj</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
