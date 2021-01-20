e3a8bea394bc382f52a64ab120585ef96368331b
kacperszyca <kacperszyca11@gmail.com>
2021-01-21 00:18:12 +0100

    Zad2

--- src/App.js
+++ src/App.js
@@ -2,33 +2,83 @@ import React, {useRef, useState} from 'react';
 import './App.css';
 
 function App() {
-  const [number, setNumber] = useState(0);
-  const [amount, setAmount] = useState(0);
-  const [iteration, setIteration] = useState(0);
-  const numberRef = useRef();
+    const [newText, setNewText] = useState("");
+    const [newText2, setNewText2] = useState("");
+    const [newRadioButton, setNewRadioButton] = useState("");
+    const [newCheck, setNewCheck] = useState("");
+    const [newDropDown, setNewDropDown] = useState("");
 
+    const text = useRef();
+    const text2 = useRef();
+    const radioButton = useRef();
+    const check = useRef();
+    const dropDown = useRef();
 
-  const submit = event =>
-  {
-    event.preventDefault();
-    setIteration(iteration+1);
-    setNumber(parseInt(numberRef.current.value) + number);
-    if( number === 0)
-      setAmount(1);
-    else
-      setAmount(number / iteration);
-  }
+    const submit = event => {
+        event.preventDefault();
+        setNewText(text.current.value);
+        setNewText2(text2.current.value);
+        setNewDropDown(dropDown.current.value);
+        //setNewRadioButton(radioButton.current.value);
+    }
+
+    const handleCheck = (event) => {
+        setNewCheck(event.target.value)
+    }
+
+    const handleRadioButton = (event) => {
+        setNewRadioButton(event.target.value)
+    }
+
+    const reset = event => {
+        event.preventDefault();
+        setNewText("");
+        setNewText2("");
+        setNewRadioButton("");
+        setNewCheck("");
+    }
 
   return(
-      <div>
+     <div>
         <form onSubmit={submit}>
-          Liczba:
-          <input type="number" pattern="[0-9]*" ref={numberRef} required={true}/>
-          <button type={"submit"}>Policz</button>
+            <input type="text" ref={text}/> <br/>
+            <input type="text" ref={text2}/> <br/>
+            <div onChange={handleRadioButton.bind(this)}>
+                <input type="radio" value="MALE" name="gender"/> Male
+                <input type="radio" value="FEMALE" name="gender"/> Female
+            </div>
+            <br/>
+            <select ref={dropDown}>
+                <option value={"Rower"}>Rower</option>
+                <option value={"Inny Rower"}>Inny rower</option>
+            </select>
+            <br/>
+            <input id={"check"} ref={check} type="checkbox" onClick={handleCheck} value={"prawda"}/> Prawda
+            <br/>
+            <button type={"submit"}>Wyslij</button> <br/>
+            <button type={"reset"} onClick={reset}>Reset</button>
         </form>
-        <h3>Suma: {number}</h3>
-        <h3>Średnia: {amount}</h3>
-      </div>
+         <table>
+             <thead>
+                 <tr>
+                     <th>Text</th>
+                     <th>Text2</th>
+                     <th>Radio</th>
+                     <th>Drop down</th>
+                     <th>Checkbox</th>
+                 </tr>
+             </thead>
+             <tbody>
+                 <tr>
+                     <td>{newText}</td>
+                     <td>{newText2}</td>
+                     <td>{newRadioButton}</td>
+                     <td>{newDropDown}</td>
+                     <td>{newCheck}</td>
+                 </tr>
+             </tbody>
+         </table>
+     </div>
   );
 }
 
